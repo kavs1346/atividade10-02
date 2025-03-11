9d4139358da448fc45d5a1868b9b1023dc7b67b3
Kauan <130669072+kavs1346@users.noreply.github.com>
2025-03-11 01:36:32 -0300

    Kavs

--- teste1.c
+++ teste1.c
@@ -3,7 +3,7 @@
 int main() {
     int x = 10;
 
-    // IF e IF-ELSE
+    
     if (x > 5) {
         printf("X é maior que 5\n");
     }
@@ -15,19 +15,19 @@ int main() {
         printf("Y é ímpar\n");
     }
 
-    // While Loop
+
     int a = 1;
     while (a <= 5) {
         printf("%d\n", a);
         a++;
     }
 
-    // For Loop
+
     for (int i = 1; i <= 5; i++) {
         printf("%d\n", i);
     }
 
-    // Do-While Loop
+
     int b = 1;
     do {
         printf("%d\n", b);
--- teste1.py
+++ teste1.py
@@ -1,4 +1,4 @@
-# IF e IF-ELSE
+
 x = 10
 if x > 5:
     print("X é maior que 5")
@@ -9,12 +9,12 @@ if y % 2 == 0:
 else:
     print("Y é ímpar")
 
-# While Loop
+
 a = 1
 while a <= 5:
     print(a)
     a += 1
 
-# For Loop
+
 for i in range(1, 6):
         print(i)
